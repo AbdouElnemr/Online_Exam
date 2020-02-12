d12361917c0f78eddc028c689537782d99a32c25
AbdouElnemr <abdouelnemr91@gmail.com>
2020-02-12 14:21:48 +0200

    add urls and templates

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authinticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('exam:exam'))
+
+    return render(request, 'account/login.html')
+
+
+def register(request):
+
+    return render(request, 'account/register.html')
